bf85f18249bf9c5d0b421be629d4ba604b74cb1a
Gustav Bylund <maistho@gmail.com>
2013-11-18 02:03:39 +0100

    Moved BugpickerFragment to new file

---
+++ app/Debugger/src/main/java/com/debugger/BugpickerFragment.java
@@ -0,0 +1,84 @@
+package com.debugger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+/**
+ * ${FILENAME} created by maistho on 11/17/13.
+ */
+
+/**
+ * A fragment containing the bugpicker view
+ */
+public class BugpickerFragment extends Fragment {
+
+    private BugpickerFragment() {
+    }
+
+    public static BugpickerFragment newInstance() {
+        BugpickerFragment fragment = new BugpickerFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_bugpicker, container, false);
+
+        Spinner language_spinner = (Spinner) rootView.findViewById(R.id.language_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.language_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        language_spinner.setAdapter(adapter);
+
+        Spinner difficulty_spinner = (Spinner) rootView.findViewById(R.id.difficulty_spinner);
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.difficulty_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficulty_spinner.setAdapter(adapter);
+
+        Switch random_switch = (Switch) rootView.findViewById(R.id.randomness_switch);
+        random_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    rootView.findViewById(R.id.random_bug).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.chosen_bug).setVisibility(View.VISIBLE);
+                } else {
+                    rootView.findViewById(R.id.chosen_bug).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.random_bug).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        Button new_bug_btn = (Button) rootView.findViewById(R.id.new_bug_btn);
+        new_bug_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bug bug = getNewBug(0);
+                Fragment fragment = EditorFragment.newInstance(bug);
+                // update the main content by replacing fragments
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment)
+                        .commit();
+            }
+        });
+        return rootView;
+    }
+
+    private Bug getNewBug(int id) {
+        return new Bug(id);
+    }
+}
--- app/Debugger/src/main/java/com/debugger/MainActivity.java
+++ app/Debugger/src/main/java/com/debugger/MainActivity.java
@@ -12,10 +12,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity
@@ -63,11 +59,11 @@ public class MainActivity extends ActionBarActivity
                 .commit();
     }
 
-    public void onSectionAttached(int number) {
+    void onSectionAttached(int number) {
         mTitle = getResources().getStringArray(R.array.nav_drawer_items)[number];
     }
 
-    public void restoreActionBar() {
+    void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
@@ -99,53 +95,11 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A fragment containing the bugpicker view
-     */
-    public static class BugpickerFragment extends Fragment {
-
-        public BugpickerFragment() {
-        }
-
-        public static BugpickerFragment newInstance() {
-            BugpickerFragment fragment = new BugpickerFragment();
-            Bundle args = new Bundle();
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            final View rootView = inflater.inflate(R.layout.fragment_bugpicker, container, false);
-
-            Spinner language_spinner = (Spinner) rootView.findViewById(R.id.language_spinner);
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.language_spinner, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            language_spinner.setAdapter(adapter);
-
-            Spinner difficulty_spinner = (Spinner) rootView.findViewById(R.id.difficulty_spinner);
-            adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.difficulty_spinner, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            difficulty_spinner.setAdapter(adapter);
-
-            Switch random_switch = (Switch) rootView.findViewById(R.id.randomness_switch);
-            random_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    if (b) {
-                        rootView.findViewById(R.id.random_bug).setVisibility(View.GONE);
-                        rootView.findViewById(R.id.chosen_bug).setVisibility(View.VISIBLE);
-                    } else {
-                        rootView.findViewById(R.id.chosen_bug).setVisibility(View.GONE);
-                        rootView.findViewById(R.id.random_bug).setVisibility(View.VISIBLE);
-                    }
-                }
-            });
-            return rootView;
-        }
+    public void swapMainFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 
     /**
@@ -177,7 +131,7 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.code_field); //TODO: Make this not cause nullptrException.
+            TextView textView = (TextView) rootView.findViewById(R.id.placeholder_field);
             textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
@@ -189,5 +143,4 @@ public class MainActivity extends ActionBarActivity
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
-
 }
