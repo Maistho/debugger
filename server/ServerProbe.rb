7665aecce2c1b58a1bca06ba7df7b170fa0d1c06
Rickard <rickie26k@gmail.com>
2013-11-26 22:19:41 +0100

    Basic HTTP server and a file to run testrequests

--- server/ServerProbe.rb
+++ server/ServerProbe.rb
@@ -1,7 +1,22 @@
 require 'net/http'
+require 'socket'
 require 'uri'
 
-Net::HTTP.post_form URI('http://localhost:8008/probe'),
-{ "q" => "ruby", "max" => "50"}
+include Socket::Constants
 
-Net::HTTP.get(URI('http://localhost:8008/probe'))
+socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+
+sockaddr = Socket.pack_sockaddr_in(8008,'localhost')
+
+socket.connect(sockaddr)
+
+socket.write( "UDBQ smurfelina\r\n\r\n")
+
+results = socket.read
+
+socket.close
+
+#Net::HTTP.get(URI('http://localhost:8008/probe'))
+
+#Net::HTTP.post_form(URI('http://localhost:8008/probe'),{ "q" => "ruby", "max" => "50"})
+
