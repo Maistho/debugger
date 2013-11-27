dcfe9339351d5bb65a639ba04d28d5373db1c0da
Rickard <rickie26k@gmail.com>
2013-11-27 21:38:29 +0100

    Basic HTTP server and a file to run testrequests

---
+++ app/Debugger/src/main/java/com/debugger/HTTPcom.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+
+public class HTTPcom {
+
+    public String ConvertforServer(String source, String[] args){
+
+        return null;
+    }
+
+    public String[] ServerCall(String call){
+
+        return null;
+    }
+}
